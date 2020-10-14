aa0660e16f82c9bfa5a5309ced17a657db288c4d
Michael Jacobsen <mic.jacobsen@gmail.com>
2020-10-14 15:08:06 +0200

    Add two color mode and multiple LEDs
    Respond to broadcast queries about IP address

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,20 @@
 #define LED_PIN 5
 #define COLOR_ORDER GRB
 #define CHIPSET WS2812B
-#define NUM_LEDS 1
 
-#define BRIGHTNESS 250
+#define WIFI_CONNECTED_LED 2
+#define SETUP_RUNNING_LED 16
+
+#define NUM_LEDS 15
+#define NUM_GROUP1 12
+
+#define BRIGHTNESS 150
 #define FRAMES_PER_SECOND 10
 
 const uint16 udp_port = 25000;
 
 WiFiUDP udp;
-
+IPAddress local_ip;
 CRGB leds[NUM_LEDS];
 
 void setup()
@@ -25,30 +30,33 @@ void setup()
   WiFi.hostname(host_name);
   WiFi.begin(ssid, password);
 
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
+  pinMode(WIFI_CONNECTED_LED, OUTPUT);
+  pinMode(SETUP_RUNNING_LED, OUTPUT);
+  digitalWrite(WIFI_CONNECTED_LED, HIGH);
+  digitalWrite(SETUP_RUNNING_LED, HIGH);
   // put your setup code here, to run once:
   delay(3000); // sanity delay
+
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
-  digitalWrite(4, LOW);
+
   int count = 0;
   while (WiFi.status() != WL_CONNECTED)
   { // Wait for the Wi-Fi to connect
     delay(500);
     Serial.print('.');
-    leds[0] = (count % 2) == 0 ? CRGB::Red : CRGB::Blue;
-    digitalWrite(4, (count % 2) == 0 ? LOW : HIGH);
-    FastLED.show();
+    digitalWrite(WIFI_CONNECTED_LED, (count % 2) == 0 ? LOW : HIGH);
     count++;
   }
-  leds[0] = CRGB::Green;
-  FastLED.show();
+
+  digitalWrite(WIFI_CONNECTED_LED, HIGH); // The LED is inverted
+  digitalWrite(SETUP_RUNNING_LED, HIGH);  // The LED is inverted
 
   Serial.println('\n');
   Serial.println("Connection established!");
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
+  local_ip = WiFi.localIP();
 
   udp.begin(udp_port);
   Serial.println('\n');
@@ -56,24 +64,58 @@ void setup()
   Serial.println(udp_port);
 }
 
-int hue = 0;
-unsigned char buffer[1024];
+void setAllSameColor(byte red, byte green, byte blue)
+{
+  FastLED.showColor(CRGB(red, green, blue));
+}
 
+void setColorsTwoSets(CRGB color1, CRGB color2)
+{
+  for (int index = 0; index < NUM_LEDS; index++)
+  {
+    leds[index] = (index < NUM_GROUP1) ? color1 : color2;
+  }
+  FastLED.show();
+}
+
+unsigned char buffer[1024];
 void loop()
 {
   int packet_size = udp.parsePacket();
   if (packet_size > 0)
   {
-    Serial.println("Got packet");
+    digitalWrite(WIFI_CONNECTED_LED, LOW);
     int length = udp.read(buffer, 1024);
-    if (length >= 3)
+    IPAddress remote = udp.remoteIP();
+    int command = buffer[0];
+    Serial.print("Got command ");
+    Serial.print(command);
+    Serial.print(" length ");
+    Serial.println(length);
+    
+    switch (command)
     {
-      for (int idx = 0; idx < NUM_LEDS; idx++)
-      {
-        leds[idx] = CRGB(buffer[0], buffer[1], buffer[2]);
-      }
-      FastLED.show();
+    case 0:
+    {
+      udp.beginPacket(remote, udp_port);
+      udp.write((const char*) &(local_ip.v4()), 4);
+      udp.endPacket();
     }
+    case 1:
+      if (length == 4)
+      {
+        setAllSameColor(buffer[1], buffer[2], buffer[3]);
+      }
+      break;
+    case 2:
+      if (length == 7)
+      {
+        setColorsTwoSets(CRGB(buffer[1], buffer[2], buffer[3]),
+                         CRGB(buffer[4], buffer[5], buffer[6]));
+      }
+      break;
+    }
+    digitalWrite(WIFI_CONNECTED_LED, HIGH);
   }
   delay(50);
 }
